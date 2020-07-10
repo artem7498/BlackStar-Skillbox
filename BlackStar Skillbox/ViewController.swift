a9e4a2f17ead5e8381b21e4e098a303e1e9141f1
Artem Akopian <artem7498@gmail.com>
2020-07-10 15:39:03 +1000

    Initial Commit

---
+++ BlackStar Skillbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlackStar Skillbox
+//
+//  Created by Артём on 7/10/20.
+//  Copyright © 2020 Artem A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
